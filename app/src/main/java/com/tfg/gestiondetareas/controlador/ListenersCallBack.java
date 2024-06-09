cefedcb60226469b643dbe87725ac97af6aaed85
victorlozano1 <147172218+victorlozano1@users.noreply.github.com>
2024-06-09 18:08:28 +0200

    Se añadio el cambio de idioma y el usuario puede marcar las tareas como completadas

---
+++ app/src/main/java/com/tfg/gestiondetareas/controlador/ListenersCallBack.java
@@ -0,0 +1,7 @@
+package com.tfg.gestiondetareas.controlador;
+
+import com.google.firebase.database.ValueEventListener;
+
+public interface ListenersCallBack {
+    void listenerObtenido(ValueEventListener listener);
+}
